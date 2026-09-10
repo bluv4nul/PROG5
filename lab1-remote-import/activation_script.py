cb5b1e770fdfe8855cff97fafa79b56b06bf01a0
Andrew <andrewkafrolov76@gmail.com>
2026-09-10 13:45:47 +0300

    requests

--- lab1-remote-import/activation_script.py
+++ lab1-remote-import/activation_script.py
@@ -4,7 +4,8 @@ import re
 import sys
 from importlib.abc import PathEntryFinder
 from importlib.util import spec_from_loader
-from urllib.request import urlopen
+
+import requests
 
 
 class URLLoader:
@@ -16,8 +17,10 @@ class URLLoader:
 
     def exec_module(self, module):
         """Качает код по origin и выполняет его в пространстве имён модуля."""
-        with urlopen(module.__spec__.origin) as page:
-            source = page.read()
+
+        response = requests.get(module.__spec__.origin, timeout=(2, 10))
+        response.raise_for_status()
+        source = response.content
         code = compile(source, module.__spec__.origin, mode="exec")
         exec(code, module.__dict__)
 
@@ -43,14 +46,14 @@ class URLFinder(PathEntryFinder):
 
 def url_hook(some_str):
     """Для http-элемента sys.path строит URLFinder, иначе ImportError."""
-    if not some_str.startswith(("http", "https")):
+    if not some_str.startswith("http"):
         raise ImportError
-    with urlopen(some_str) as page:  # requests.get()
-        data = page.read().decode("utf-8")
+    response = requests.get(some_str, timeout=(2, 10))
+    response.raise_for_status()
+    data = response.text
     filenames = re.findall("[a-zA-Z_][a-zA-Z0-9_]*.py", data)
     modnames = {name[:-3] for name in filenames}
     return URLFinder(some_str, modnames)
 
 
 sys.path_hooks.append(url_hook)
-print(sys.path_hooks)
